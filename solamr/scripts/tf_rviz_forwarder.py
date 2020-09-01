94059904b2eacb8019ea8b48db5109d416bae20a
Your Name <you@example.com>
2020-09-01 16:40:30 +0800

    tf rviz forwarding yaml

--- solamr/scripts/tf_rviz_forwarder.py
+++ solamr/scripts/tf_rviz_forwarder.py
@@ -15,32 +15,33 @@ if __name__ == '__main__':
     pub_tf = rospy.Publisher("/tf_rviz", tf.msg.tfMessage, queue_size = 10)
     
     # Get launch file parameters
-    ROBOT_NAME = rospy.get_param(param_name="~robot_name", default="car1")
-
+    # ROBOT_NAME = rospy.get_param(param_name="~robot_name", default="car1")
+    TF_RVIZ_LIST = rospy.get_param(param_name="~tf_rviz_list")
+    # rospy.logerr("[tf_rviz_forwarder] " + str(TF_RVIZ_LIST))
     rate = rospy.Rate(10.0)
 
-    tf_request_list = ((ROBOT_NAME + "/map", ROBOT_NAME + "/odom"),
-                       (ROBOT_NAME + "/odom", ROBOT_NAME + "/base_link"),
-                       (ROBOT_NAME + "/base_link", ROBOT_NAME + "/laser_front"),
-                       (ROBOT_NAME + "/base_link", ROBOT_NAME + "/laser_rear"),
+    # tf_request_list = ((ROBOT_NAME + "/map", ROBOT_NAME + "/odom"),
+    #                    (ROBOT_NAME + "/odom", ROBOT_NAME + "/base_link"),
+    #                    (ROBOT_NAME + "/base_link", ROBOT_NAME + "/laser_front"),
+    #                    (ROBOT_NAME + "/base_link", ROBOT_NAME + "/laser_rear"),
 
-                       #(ROBOT_NAME + "map", ROBOT_NAME + "s_center_laser"),
-                       #(ROBOT_NAME + "map", ROBOT_NAME + "center_peer"),
-                       #(ROBOT_NAME + "map", ROBOT_NAME + "center_big_car"),
-                       #(ROBOT_NAME + "s_center_laser", ROBOT_NAME + "s_standby_laser"),
-                       ("carB/map", "carB/odom"),
-                       ("carB/odom", "carB/base_link"),
-                       ("carB/base_link", "car1/base_link"),
-                       ("carB/base_link", "car2/base_link"),
+    #                    #(ROBOT_NAME + "map", ROBOT_NAME + "s_center_laser"),
+    #                    #(ROBOT_NAME + "map", ROBOT_NAME + "center_peer"),
+    #                    #(ROBOT_NAME + "map", ROBOT_NAME + "center_big_car"),
+    #                    #(ROBOT_NAME + "s_center_laser", ROBOT_NAME + "s_standby_laser"),
+    #                    ("carB/map", "carB/odom"),
+    #                    ("carB/odom", "carB/base_link"),
+    #                    ("carB/base_link", "car1/base_link"),
+    #                    ("carB/base_link", "car2/base_link"),
 
-                       (ROBOT_NAME + "/base_link", ROBOT_NAME + "/s_front"),
-                       (ROBOT_NAME + "/s_front", ROBOT_NAME + "/s_standby_camera"),
-                       (ROBOT_NAME + "/s_front", ROBOT_NAME + "/s_center_camera"))
+    #                    (ROBOT_NAME + "/base_link", ROBOT_NAME + "/s_front"),
+    #                    (ROBOT_NAME + "/s_front", ROBOT_NAME + "/s_standby_camera"),
+    #                    (ROBOT_NAME + "/s_front", ROBOT_NAME + "/s_center_camera"))
 
 
     while not rospy.is_shutdown():
         tf_list = []
-        for i in tf_request_list:
+        for i in TF_RVIZ_LIST:
             try:
                 t = tfBuffer.lookup_transform(i[0], i[1], rospy.Time())
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
