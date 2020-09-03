99ae674535bc77734ef9932ae5b6471866bf68e6
spiderkiller <luckyyu910645@gmail.com>
2020-09-03 23:48:42 +0800

    finish flow structure

--- solamr/scripts/fsm.py
+++ solamr/scripts/fsm.py
@@ -47,6 +47,28 @@ def init_roslaunch(file_path):
     roslaunch.configure_logging(uuid=uuid)
     return roslaunch.parent.ROSLaunchParent(run_id=uuid, roslaunch_files=((file_path,)))
 
+def switch_launch_double():
+    '''
+    '''
+    global ROSLAUNCH
+    if ROSLAUNCH != None:
+        rospy.loginfo("[fsm] Shuting down single_AMR launch file")
+        ROSLAUNCH.shutdown()
+        time.sleep(5) # TODO DO WE need it ?
+    rospy.loginfo("[fsm] Start launch file: " + ROSLAUNCH_PATH_DOUBLE_AMR)
+    ROSLAUNCH = init_roslaunch(ROSLAUNCH_PATH_DOUBLE_AMR)
+    ROSLAUNCH.start()
+
+def switch_launch_single():
+    global ROSLAUNCH
+    if ROSLAUNCH != None:
+        rospy.loginfo("[fsm] Shuting down double_AMR launch file")
+        ROSLAUNCH.shutdown()
+        time.sleep(5) # TODO DO WE need it ?
+    rospy.loginfo("[fsm] Start launch file: " + ROSLAUNCH_PATH_SINGLE_AMR)
+    ROSLAUNCH = init_roslaunch(ROSLAUNCH_PATH_SINGLE_AMR)
+    ROSLAUNCH.start()
+
 def task_cb(req):
     '''
     Load yaml file and change parameter by req.data file path
@@ -58,7 +80,8 @@ def task_cb(req):
         rospy.logerr("[fsm] Reject task, because I'm busy now.")
         return "Reject task, I'm busy now"
     
-    path = rospkg.RosPack().get_path('solamr') + "/params/task/" + req.data
+    path = rospkg.RosPack().get_path('solamr') + "/params/task/" + req.data\
+           + "_" + ROBOT_NAME + ".yaml"
     try:
         with open(path) as file:
             rospy.loginfo("[shelf_detector] Load yaml file from " + path)
@@ -74,25 +97,31 @@ def task_cb(req):
     except OSError:
         rospy.logerr("[fsm] Yaml file not found at " + req.data)
         return "Yaml file not found"
+class Initial_State(smach.State):
+    def __init__(self):
+        super(Initial_State, self).__init__(outcomes=('Single_AMR', 'Single_Assembled', 'Double_Assembled'))
+
+    def execute(self, userdata):
+        if INIT_STATE == 'Single_AMR' or INIT_STATE == 'Single_Assembled':
+            switch_launch_single()
+        elif INIT_STATE == 'Double_Assembled':
+            switch_launch_double()
+        return INIT_STATE
 
 class Single_AMR(smach.State):
-
+    '''
+    Waiting State, do nothing
+    '''
     def __init__(self, file_path):
         super(Single_AMR, self).__init__(outcomes=('Find_Shelf', 'done'), output_keys=[])
         self.launch_file = file_path
 
     def execute(self, userdata):
-        roslaunch = init_roslaunch(self.launch_file)
-        roslaunch.start()
-        # roslaunch.shutdown()
         rospy.loginfo('[fsm] Execute Single_AMR')
         while not rospy.is_shutdown():
             if TASK != None:
                 break
             time.sleep(1)
-        print (TASK.mode)
-        # TODO Dark Magic, Change footprint back to single AMR
-        os.system("rostopic pub --once /move_base/local_costmap/footprint geometry_msgs/Polygon -- '[[-0.22,-0.22,0.0], [-0.22,0.22,0.0], [0.22,0.22,0.0], [0.22,-0.22,0.0]]'")
         return 'Find_Shelf'
 
 class Find_Shelf(smach.State):
@@ -108,7 +137,7 @@ class Find_Shelf(smach.State):
         # TODO Listen to service abort
         rospy.loginfo('[fsm] Execute Find_Shelf')
         rospy.loginfo(str(TASK.mode))
-        # TODO send_goal (userdata.proximity_goal)
+        # TODO send_goal
         # Keep checking tagid, and make sure standby tf is published
         time.sleep(1)
         return 'done'
@@ -134,11 +163,33 @@ class Dock_In(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('[fsm] Execute Dock_In')
-        # TODO # Switch launch file, You have to know you're car1/car2 at this point
-        # TODO How to decide goto Single_Assembled or Double_Assembled
-        # If IR said it's done or navigation goal is reached
+        # TODO If IR said it's done or navigation goal is reached
         time.sleep(1)
-        return 'Single_Assembled'
+        if TASK.mode == "single_AMR":
+            # TODO Dark Magic, Change footprint to single Assembled
+            os.system("rostopic pub --once /move_base/local_costmap/footprint geometry_msgs/Polygon -- '[[-0.37,-0.37,0.0], [-0.37,0.37,0.0], [0.37,0.37,0.0], [0.37,-0.37,0.0]]'")
+            return 'Single_Assembled'
+        elif TASK.mode == "double_AMR":
+            switch_launch_double()
+            return 'Double_Assembled'
+
+class Go_Way_Point(smach.State):
+    def __init__(self):
+        super(Go_Way_Point, self).__init__(outcomes=['done', 'abort'], input_keys=["target"], output_keys=["behavior"])
+
+    def execute(self, userdata):
+        rospy.loginfo('[fsm] Execute Go_Way_Point')
+        time.sleep(1)
+        return 'done'
+
+class Go_Goal(smach.State):
+    def __init__(self):
+        super(Go_Goal, self).__init__(outcomes=['done', 'abort'], input_keys=["target"], output_keys=["behavior"])
+
+    def execute(self, userdata):
+        rospy.loginfo('[fsm] Execute Go_Goal')
+        time.sleep(1)
+        return 'done'
 
 class Dock_Out(smach.State):
 
@@ -153,7 +204,24 @@ class Dock_Out(smach.State):
         # Switch launch file 
         # TODO How to decide goto Single_Assembled or Double_Assembled
         # TODO reached dock_out navi goal
+        if TASK.mode == "single_AMR":
+            # TODO Dark Magic, Change footprint back to single AMR
+            os.system("rostopic pub --once /move_base/local_costmap/footprint geometry_msgs/Polygon -- '[[-0.22,-0.22,0.0], [-0.22,0.22,0.0], [0.22,0.22,0.0], [0.22,-0.22,0.0]]'")
+        elif TASK.mode == "double_AMR":
+            switch_launch_single()
+
         time.sleep(1)
+        return 'done'
+
+class Go_Home(smach.State):
+    def __init__(self):
+        super(Go_Home, self).__init__(outcomes=['done', 'abort'], input_keys=["target"], output_keys=["behavior"])
+
+    def execute(self, userdata):
+        global TASK
+        rospy.loginfo('[fsm] Execute Go_Home')
+        time.sleep(1)
+        TASK = None
         return 'done'
 
 class Single_Assembled(smach.State):
@@ -163,11 +231,15 @@ class Single_Assembled(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('[fsm] Execute Single_Assembled')
-        
-        # TODO Dark Magic, Change footprint to single Assembled
-        os.system("rostopic pub --once /move_base/local_costmap/footprint geometry_msgs/Polygon -- '[[-0.37,-0.37,0.0], [-0.37,0.37,0.0], [0.37,0.37,0.0], [0.37,-0.37,0.0]]'")
-        time.sleep(5)
-        return 'Go_Way_Point'
+        # TODO rosservice to dock out or accept a task
+        while not rospy.is_shutdown():
+            if TASK == None:
+                # TODO listen to service to decide
+                continue
+                return 'Dock_Out'
+            else:
+                return 'Go_Way_Point'
+            time.sleep(1)
         '''
         x_m, y_m, rz_deg, behavior = userdata.target
         userdata.behavior = behavior
@@ -192,47 +264,23 @@ class Single_Assembled(smach.State):
             return 'failed'
         '''
 
-class Go_Way_Point(smach.State):
-    def __init__(self):
-        super(Go_Way_Point, self).__init__(outcomes=['done', 'abort'], input_keys=["target"], output_keys=["behavior"])
-
-    def execute(self, userdata):
-        rospy.loginfo('[fsm] Execute Go_Way_Point')
-        time.sleep(1)
-        return 'done'
-
-class Go_Goal(smach.State):
-    def __init__(self):
-        super(Go_Goal, self).__init__(outcomes=['done', 'abort'], input_keys=["target"], output_keys=["behavior"])
-
-    def execute(self, userdata):
-        rospy.loginfo('[fsm] Execute Go_Goal')
-        time.sleep(1)
-        return 'done'
-
-class Go_Home(smach.State):
-    def __init__(self):
-        super(Go_Home, self).__init__(outcomes=['done', 'abort'], input_keys=["target"], output_keys=["behavior"])
-
-    def execute(self, userdata):
-        rospy.loginfo('[fsm] Execute Go_Home')
-        time.sleep(1)
-        return 'done'
-
 class Double_Assembled(smach.State):
 
     def __init__(self, file_path):
-        super(Double_Assembled, self).__init__(outcomes=['Dock_Out'], output_keys=["target"])
-        self.launch_file = file_path
+        super(Double_Assembled, self).__init__(outcomes=['Dock_Out', 'Go_Way_Point'], output_keys=["target"])
 
     def execute(self, userdata):
         rospy.loginfo('[fsm] Execute Double_Assembled')
+        while not rospy.is_shutdown():
+            if TASK == None:
+                # TODO listen to service to decide
+                continue
+                return 'Dock_Out'
+            else:
+                return 'Go_Way_Point'
+            time.sleep(1)
 
-        roslaunch = init_roslaunch(self.launch_file)
-        roslaunch.start()
-        time.sleep(5)
-        # roslaunch.shutdown()
-        return 'Dock_Out'
+
 
 if __name__ == "__main__":
 
@@ -243,12 +291,14 @@ if __name__ == "__main__":
     ROSLAUNCH_PATH_DOUBLE_AMR = rospy.get_param(param_name="~roslaunch_path_double_amr")
     ROBOT_NAME = rospy.get_param(param_name="~robot_name")
     ROLE = rospy.get_param(param_name="~role")
+    INIT_STATE = rospy.get_param(param_name="~init_state")
 
     # Service
     rospy.Service(name="~task", service_class=StringSrv, handler=task_cb)
 
     # Global variable 
     TASK = None # store task information
+    ROSLAUNCH = None
     # -- Get service
     # action_move_base = actionlib.SimpleActionClient(ns=ns_action, ActionSpec=MoveBaseAction)
     # action_move_base.wait_for_server()
@@ -256,6 +306,13 @@ if __name__ == "__main__":
     # -- FSM Config
     SM = smach.StateMachine(outcomes=['completed'])
     with SM:
+        smach.StateMachine.add(
+            label='Initial_State',
+            state=Initial_State(),
+            transitions={'Single_AMR': 'Single_AMR',
+                         'Single_Assembled': 'Single_Assembled',
+                         'Double_Assembled': 'Double_Assembled'})
+        
         smach.StateMachine.add(
             label='Single_AMR',
             state=Single_AMR(file_path=ROSLAUNCH_PATH_SINGLE_AMR),
@@ -314,7 +371,7 @@ if __name__ == "__main__":
         smach.StateMachine.add(
             label='Double_Assembled',
             state=Double_Assembled(file_path=ROSLAUNCH_PATH_DOUBLE_AMR),
-            transitions={'Dock_Out': 'Dock_Out'})
+            transitions={'Dock_Out': 'Dock_Out', 'Go_Way_Point': 'Go_Way_Point'})
     
     # -- FSM VIEWER
     sis = smach_ros.IntrospectionServer('smach_server', SM, '/ROOT')
