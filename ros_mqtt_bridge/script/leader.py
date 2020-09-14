559de66b442ec89530aaea46f878eb1be3d4e72f
Your Name <you@example.com>
2020-09-14 15:58:16 +0800

    use mqtt to pass cureeent state

--- ros_mqtt_bridge/script/leader.py
+++ ros_mqtt_bridge/script/leader.py
@@ -13,7 +13,9 @@ def main(args):
     # init ros_mqtt_bridge
     ros_mqtt_bridge = Ros_mqtt_bridge(client_id="mqtt_leader", broker_ip="10.0.0.1", port=1883, keepalive=10, clean_session=True)
     ros_mqtt_bridge.init_publisher("/car2/cmd_vel", "p2p_cmd", "geometry_msgs/Twist")
+    ros_mqtt_bridge.init_publisher("/car1/current_state", "car1_current_state", "std_msgs/String")
     ros_mqtt_bridge.init_subscriber("/car2/theta", "car2_theta", "std_msgs/Float64")
+    ros_mqtt_bridge.init_subscriber("/car2/current_state", "car2_current_state", "std_msgs/String")
     ros_mqtt_bridge.init_tf_subscriber(frame_id = "car2/raw/map", child_id = "car2/raw/odom",mqtt_topic="mqtt_tf_1")
     ros_mqtt_bridge.init_tf_subscriber(frame_id = "car2/raw/odom", child_id = "car2/raw/base_link",mqtt_topic="mqtt_tf_3")
     r = rospy.Rate(10) #call at 50HZ # Need to be faster than tf hz 
